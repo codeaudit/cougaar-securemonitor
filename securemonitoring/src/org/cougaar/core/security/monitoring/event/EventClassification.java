11d979525a497853f7bf898a65943eb7bc62e397
srosset <srosset>
2004-08-13 23:17:11 +0000

    secure monitoring & response

---
+++ securemonitoring/src/org/cougaar/core/security/monitoring/event/EventClassification.java
@@ -0,0 +1,48 @@
+/* 
+ * <copyright> 
+ *  Copyright 1999-2004 Cougaar Software, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects 
+ *  Agency (DARPA). 
+ *  
+ *  You can redistribute this software and/or modify it under the
+ *  terms of the Cougaar Open Source License as published on the
+ *  Cougaar Open Source Website (www.cougaar.org).  
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * </copyright> 
+ */ 
+
+package org.cougaar.core.security.monitoring.event;
+
+import org.cougaar.core.security.constants.IdmefClassifications;
+
+import edu.jhuapl.idmef.Classification;
+
+/**
+ * @author srosset
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class EventClassification
+{
+  public static final Classification LOGINFAILURE =
+    new Classification(IdmefClassifications.LOGIN_FAILURE, "", Classification.VENDOR_SPECIFIC);
+
+  public static final Classification SECURITY_MGR_EXCEPTION =
+    new Classification(IdmefClassifications.SECURITY_MANAGER_EXCEPTION, "", Classification.VENDOR_SPECIFIC);
+  public static final Classification JAR_VERIFY_FAILURE =
+    new Classification(IdmefClassifications.JAR_VERIFICATION_FAILURE, "", Classification.VENDOR_SPECIFIC);
+
+}
